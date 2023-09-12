74a2025c5b2a9c8d4125394dee1102581b2ef333
brnaba <brnaba@amazon.fr>
2023-09-12 23:36:42 +0200

    added support for stream manager custom port

--- src/main/java/com/aws/greengrass/localdebugconsole/StreamManagerHelper.java
+++ src/main/java/com/aws/greengrass/localdebugconsole/StreamManagerHelper.java
@@ -7,30 +7,57 @@ import java.util.List;
 import com.amazonaws.greengrass.streammanager.client.StreamManagerClientFactory;
 import com.amazonaws.greengrass.streammanager.client.config.StreamManagerAuthInfo;
 import com.amazonaws.greengrass.streammanager.client.config.StreamManagerClientConfig;
+import com.amazonaws.greengrass.streammanager.client.config.StreamManagerServerInfo;
 import com.amazonaws.greengrass.streammanager.model.Message;
 import com.amazonaws.greengrass.streammanager.model.MessageStreamInfo;
 import com.amazonaws.greengrass.streammanager.client.StreamManagerClient;
 import com.amazonaws.greengrass.streammanager.model.ReadMessagesOptions;
+import com.aws.greengrass.config.Topic;
+import com.aws.greengrass.config.Topics;
+import com.aws.greengrass.lifecyclemanager.Kernel;
 import com.aws.greengrass.logging.api.Logger;
 import com.aws.greengrass.logging.impl.LogManager;
+import com.aws.greengrass.util.Coerce;
+
+import static com.aws.greengrass.authorization.AuthorizationIPCAgent.STREAM_MANAGER_SERVICE_NAME;
+import static com.aws.greengrass.componentmanager.KernelConfigResolver.CONFIGURATION_CONFIG_KEY;
 
 public class StreamManagerHelper {
 
+    private static final String STREAM_MANAGER_PORT_KEY = "STREAM_MANAGER_SERVER_PORT";
     private final Logger logger;
     private StreamManagerClient client;
     private final String streamManagerAuthToken;
 
     private Boolean isConnected;
 
-    public StreamManagerHelper(String authToken) {
+    private String streamManagerPort = "8088";
+
+    private final Kernel kernel;
+
+    public StreamManagerHelper(Kernel kernel, String authToken) {
         this.logger = LogManager.getLogger(StreamManagerHelper.class);
         this.streamManagerAuthToken = authToken;
         this.isConnected = false;
+        this.kernel = kernel;
     }
 
     private void connect(){
         try {
-            StreamManagerClientConfig config = StreamManagerClientConfig.builder().build();
+            Topics smTopics = this.kernel.findServiceTopic(STREAM_MANAGER_SERVICE_NAME);
+            if (smTopics == null) {
+                logger.atDebug().log("{} service topic not found", STREAM_MANAGER_SERVICE_NAME);
+            } else {
+                Topic portTopic = smTopics.find(CONFIGURATION_CONFIG_KEY, STREAM_MANAGER_PORT_KEY);
+                if (portTopic == null) {
+                    logger.atError().log("Stream Manager's ({}) port information not found", STREAM_MANAGER_SERVICE_NAME);
+                } else {
+                    this.streamManagerPort = Coerce.toString(portTopic);
+                }
+            }
+            assert this.streamManagerPort != null;
+            StreamManagerClientConfig config = StreamManagerClientConfig.builder()
+                    .serverInfo(StreamManagerServerInfo.builder().port(Integer.parseInt(this.streamManagerPort)).build()).build();
             StreamManagerAuthInfo authInfo = config.getAuthInfo();
             Field field = authInfo.getClass().getDeclaredField("authToken");
             field.setAccessible(true);
