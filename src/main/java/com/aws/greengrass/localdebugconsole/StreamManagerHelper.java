df258343b2b9a9c58dc26b6c271da7a1a4a4ab05
Michael Dombrowski <mdombro@amazon.com>
2023-09-27 13:54:11 -0400

    fix: get correct SM port, reconnect when needed

--- src/main/java/com/aws/greengrass/localdebugconsole/StreamManagerHelper.java
+++ src/main/java/com/aws/greengrass/localdebugconsole/StreamManagerHelper.java
@@ -5,6 +5,8 @@ import com.amazonaws.greengrass.streammanager.client.StreamManagerClientFactory;
 import com.amazonaws.greengrass.streammanager.client.config.StreamManagerAuthInfo;
 import com.amazonaws.greengrass.streammanager.client.config.StreamManagerClientConfig;
 import com.amazonaws.greengrass.streammanager.client.config.StreamManagerServerInfo;
+import com.amazonaws.greengrass.streammanager.client.exception.ClientClosedException;
+import com.amazonaws.greengrass.streammanager.client.exception.ConnectException;
 import com.amazonaws.greengrass.streammanager.client.exception.StreamManagerException;
 import com.amazonaws.greengrass.streammanager.model.Message;
 import com.amazonaws.greengrass.streammanager.model.MessageStreamDefinition;
@@ -25,14 +27,11 @@ import static com.aws.greengrass.componentmanager.KernelConfigResolver.CONFIGURA
 
 public class StreamManagerHelper {
 
-    private static final String STREAM_MANAGER_PORT_KEY = "STREAM_MANAGER_SERVER_PORT";
     private final Logger logger;
     private StreamManagerClient client;
     private final String streamManagerAuthToken;
 
-    private Boolean isConnected;
-
-    private String streamManagerPort = "8088";
+    private boolean isConnected;
 
     private final Kernel kernel;
 
@@ -44,22 +43,30 @@ public class StreamManagerHelper {
     }
 
     private void connect() throws StreamManagerException {
+        if (this.client != null) {
+            try {
+                this.client.close();
+            } catch (Exception e) {
+                logger.atError().log("Unable to close StreamManager client", e);
+            }
+        }
+
         try {
+            Integer streamManagerPort = null;
             Topics smTopics = this.kernel.findServiceTopic(STREAM_MANAGER_SERVICE_NAME);
             if (smTopics == null) {
                 logger.atDebug().log("{} service topic not found", STREAM_MANAGER_SERVICE_NAME);
             } else {
-                Topic portTopic = smTopics.find(CONFIGURATION_CONFIG_KEY, STREAM_MANAGER_PORT_KEY);
+                Topic portTopic = smTopics.find(CONFIGURATION_CONFIG_KEY, "port");
                 if (portTopic == null) {
-                    logger.atError()
-                            .log("Stream Manager's ({}) port information not found", STREAM_MANAGER_SERVICE_NAME);
+                    logger.atWarn().log("Stream Manager's ({}) port information not found. Will use default port",
+                            STREAM_MANAGER_SERVICE_NAME);
                 } else {
-                    this.streamManagerPort = Coerce.toString(portTopic);
+                    streamManagerPort = Coerce.toInt(portTopic);
                 }
             }
-            assert this.streamManagerPort != null;
-            StreamManagerClientConfig config = StreamManagerClientConfig.builder().serverInfo(
-                    StreamManagerServerInfo.builder().port(Integer.parseInt(this.streamManagerPort)).build()).build();
+            StreamManagerClientConfig config = StreamManagerClientConfig.builder()
+                    .serverInfo(StreamManagerServerInfo.builder().port(streamManagerPort).build()).build();
             StreamManagerAuthInfo authInfo = config.getAuthInfo();
             Field field = authInfo.getClass().getDeclaredField("authToken");
             field.setAccessible(true);
@@ -80,21 +87,39 @@ public class StreamManagerHelper {
         if (!this.isConnected) {
             this.connect();
         }
-        return this.client.listStreams();
+        try {
+            return this.client.listStreams();
+        } catch (ClientClosedException | ConnectException e) {
+            // reconnect
+            this.connect();
+            return this.client.listStreams();
+        }
     }
 
     public MessageStreamInfo describeStream(String streamName) throws StreamManagerException {
         if (!this.isConnected) {
             this.connect();
         }
-        return this.client.describeMessageStream(streamName);
+        try {
+            return this.client.describeMessageStream(streamName);
+        } catch (ClientClosedException | ConnectException e) {
+            // reconnect
+            this.connect();
+            return this.client.describeMessageStream(streamName);
+        }
     }
 
     public void deleteMessageStream(String streamName) throws StreamManagerException {
         if (!this.isConnected) {
             this.connect();
         }
-        this.client.deleteMessageStream(streamName);
+        try {
+            this.client.deleteMessageStream(streamName);
+        } catch (ClientClosedException | ConnectException e) {
+            // reconnect
+            this.connect();
+            this.client.deleteMessageStream(streamName);
+        }
     }
 
     public List<Message> readMessages(String streamName, Long desiredStartSequenceNumber, Long minMessageCount,
@@ -102,29 +127,55 @@ public class StreamManagerHelper {
         if (!this.isConnected) {
             this.connect();
         }
-        return this.client.readMessages(streamName,
-                new ReadMessagesOptions(desiredStartSequenceNumber, minMessageCount, maxMessageCount,
-                        readTimeoutMillis));
+        try {
+            return this.client.readMessages(streamName,
+                    new ReadMessagesOptions(desiredStartSequenceNumber, minMessageCount, maxMessageCount,
+                            readTimeoutMillis));
+        } catch (ClientClosedException | ConnectException e) {
+            // reconnect
+            this.connect();
+            return this.client.readMessages(streamName,
+                    new ReadMessagesOptions(desiredStartSequenceNumber, minMessageCount, maxMessageCount,
+                            readTimeoutMillis));
+        }
     }
 
     public void appendMessage(String streamName, byte[] message) throws StreamManagerException {
         if (!this.isConnected) {
             this.connect();
         }
-        this.client.appendMessage(streamName, message);
+        try {
+            this.client.appendMessage(streamName, message);
+        } catch (ClientClosedException | ConnectException e) {
+            // reconnect
+            this.connect();
+            this.client.appendMessage(streamName, message);
+        }
     }
 
     public void createMessageStream(MessageStreamDefinition messageStream) throws StreamManagerException {
         if (!this.isConnected) {
             this.connect();
         }
-        this.client.createMessageStream(messageStream);
+        try {
+            this.client.createMessageStream(messageStream);
+        } catch (ClientClosedException | ConnectException e) {
+            // reconnect
+            this.connect();
+            this.client.createMessageStream(messageStream);
+        }
     }
 
     public void updateMessageStream(MessageStreamDefinition messageStream) throws StreamManagerException {
         if (!this.isConnected) {
             this.connect();
         }
-        this.client.updateMessageStream(messageStream);
+        try {
+            this.client.updateMessageStream(messageStream);
+        } catch (ClientClosedException | ConnectException e) {
+            // reconnect
+            this.connect();
+            this.client.updateMessageStream(messageStream);
+        }
     }
 }
